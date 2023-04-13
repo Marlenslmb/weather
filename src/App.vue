6e4ef16323b4ab431b36d58993edf15842ef9f9d
Marlen <marlenslmb@mgail.com>
2023-04-13 15:07:04 +0600

    test commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <IndexPage></IndexPage>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import IndexPage from "./views/IndexPage.vue";
+</script>
---
+++ src/views/IndexPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <MainPage></MainPage>
+</template>
+
+<script lang="ts" setup>
+import MainPage from "@/modules/MainPage.vue";
+</script>
